43c16a007c342534b2678cc3d8e18156c873043d
Samuel Campos <samuelcamposprof@gmail.com>
2026-03-28 20:02:49 -0300

    Adição de Arquivos, adição de extenção com arquivos para rodar C# Código C# adicionado

---
+++ MeuPrograma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+    class MainClass {
+        public static void Main (string[] args) {
+        int n1, n2;
+        double n3, n4;
+        float n5;
+        string s;
+        bool b1, b2;
+        // Leitura de Inteiros
+        Console.Write("Digite um número inteiro: ");
+        n1 = int.Parse(Console.ReadLine());
+        Console.Write("Digite outro número inteiro: ");
+        n2 = Convert.ToInt32(Console.ReadLine());
+        // Leitura de valores ponto-flutuante
+        Console.Write("Digite um número real: ");
+        n3 = double.Parse(Console.ReadLine());
+        Console.Write("Digite outro número real: ");
+        n4 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite mais um número real: ");
+        n5 = float.Parse(Console.ReadLine());
+        Console.Write("Digite um booleano (digite true ou false): ");
+        b1 = bool.Parse(Console.ReadLine());
+        Console.WriteLine($"Inteiro 1 = {n1}\nInteiro 2 = {n2}\nReal 1 = {n3}\nReal 2 = {n4}\nReal 3 = {n5}\nBool = {b1}");
+    }
+}
